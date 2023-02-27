5c52a551efd8b39477b80b352c92f0e7fc720e68
tuzinmaibrabo <arthurquarxma@outlook.com>
2023-02-27 17:21:14 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { BsFillTrashFill, BsFillCheckCircleFill } from "react-icons/bs";
+import { BiAddToQueue } from "react-icons/bi";
+import "./App.css";
 
 function App() {
+  const [todoList, setTodoList] = useState([]);
+  const [newTask, setNewTask] = useState("");
+  const [style, setStyle] = useState();
+
+  const handleChange = (e) => {
+    setNewTask(e.target.value);
+  };
+
+  const addTask = (e) => {
+    const newTodoList = [...todoList, newTask];
+    setTodoList(newTodoList);
+    // or setTodoList([...todoList, newTask]) returns the same.
+  };
+
+  const deleteTask = (taskName) => {
+    const newTodoList = todoList.filter((task) => {
+      if (task === taskName) {
+        return false;
+      } else {
+        return true;
+      }
+    });
+    setTodoList(newTodoList);
+  };
+
+  const doneTask = () => {
+    setStyle({ textDecoration: "line-through" });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <input type="text" onChange={handleChange} />
+        <button onClick={addTask}>
+          <BiAddToQueue />
+        </button>
+      </div>
+      <div>
+        {todoList.map((task) => {
+          return (
+            <div style={style}>
+              <h1>{task}</h1>
+              <button
+                onClick={() => {
+                  deleteTask(task);
+                }}
+              >
+                <BsFillTrashFill />
+              </button>
+              <button onClick={doneTask}>
+                <BsFillCheckCircleFill />
+              </button>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
